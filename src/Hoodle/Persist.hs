d45bc6b200dd0b96e3116f19fdad741201ea4392
Ian-Woo Kim <ianwookim@gmail.com>
2013-02-01 17:42:13 -0500

    init

---
+++ src/Hoodle/Persist.hs
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      : Hoodle.Persist
+-- Copyright   : (c) 2013 Ian-Woo Kim
+--
+-- License     : BSD3
+-- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
+-- Stability   : experimental
+-- Portability : GHC
+--
+-----------------------------------------------------------------------------
+
+module Hoodle.Persist where
+
+
